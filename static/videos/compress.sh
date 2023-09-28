2f4f151ff6d4494375582f2445f4519a77a025e0
realfill <realfill2023@gmail.com>
2023-09-28 16:47:47 -0400

    fix all result video

--- static/videos/compress.sh
+++ static/videos/compress.sh
@@ -1,5 +1,6 @@
 for NAME in "bedroom" "bottleplant" "jesusheart" "castle" "flowerwoman" "horselamp" \
-    "mandaynight" "vegas" "yellow" "shibaoeat" "shibaosleep" "queensboro"
+    "mandaynight" "vegas" "yellow" "shibaoeat" "shibaosleep" "queensboro" "pumpkinlady" \
+    "roofcup"
 do
-    ffmpeg -i ${NAME}.mp4 -c:v libx264 -crf 23 -an ${NAME}_compressed.mp4
+    ffmpeg -y -i ${NAME}.mp4 -c:v libx264 -crf 23 -an ${NAME}_compressed.mp4
 done
