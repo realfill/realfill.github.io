8689c43580f5c2997aa85eda17acf9f055736e50
realfill <realfill2023@gmail.com>
2023-09-28 16:42:50 -0400

    fix flickering issue

--- static/videos/compress.sh
+++ static/videos/compress.sh
@@ -1,4 +1,5 @@
-for NAME in "castle"
+for NAME in "bedroom" "bottleplant" "jesusheart" "castle" "flowerwoman" "horselamp" \
+    "mandaynight" "vegas" "yellow" "shibaoeat" "shibaosleep" "queensboro"
 do
     ffmpeg -i ${NAME}.mp4 -c:v libx264 -crf 23 -an ${NAME}_compressed.mp4
 done
