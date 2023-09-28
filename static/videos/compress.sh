77c038de17538cd73056e1629446ceb98245723e
realfill <realfill2023@gmail.com>
2023-09-28 18:01:27 -0400

    update teaser, but has weird gray line on the bottom

--- static/videos/compress.sh
+++ static/videos/compress.sh
@@ -1,6 +1,11 @@
-for NAME in "bedroom" "bottleplant" "jesusheart" "castle" "flowerwoman" "horselamp" \
-    "mandaynight" "vegas" "yellow" "shibaoeat" "shibaosleep" "queensboro" "pumpkinlady" \
-    "roofcup"
+# for NAME in "bedroom" "bottleplant" "jesusheart" "castle" "flowerwoman" "horselamp" \
+#     "mandaynight" "vegas" "yellow" "shibaoeat" "shibaosleep" "queensboro" "pumpkinlady" \
+#     "roofcup"
+# do
+#     ffmpeg -y -i ${NAME}.mp4 -c:v libx264 -crf 23 -an ${NAME}_compressed.mp4
+# done
+
+for NAME in "teaser_boy"
 do
     ffmpeg -y -i ${NAME}.mp4 -c:v libx264 -crf 23 -an ${NAME}_compressed.mp4
 done
